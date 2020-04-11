5197c8cab509b0759f9bc853f66812dedd88142f
Nick Evans <evansnicholasa@gmail.com>
2020-04-11 02:05:48 -0400

    initial commit

---
+++ src/main/java/com/nevans/foods/models/Food.java
@@ -0,0 +1,16 @@
+package com.nevans.foods.models;
+
+public class Food {
+
+    private long id = -1;
+
+    private String name = "";
+    private String added_date = "";
+    private String purchase_date = "";
+    private String marked_expiration_date = "";
+    private String artificial_expiration_date = "";
+    private String description = "";
+    private boolean refrigerated = false;
+    private boolean frozen = false;
+    private double est_cost = 0.0;
+}
